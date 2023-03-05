324d5187da5ac36cfb94fa92f9d16fad09f85863
Peter Tsanev <78775944+tsanevp@users.noreply.github.com>
2023-03-05 21:52:34 +0000

    cleaned up the CreateListingCommand and NewGuildJoined

--- src/main/java/edu/northeastern/cs5500/starterbot/command/NewGuildJoined.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/command/NewGuildJoined.java
@@ -29,12 +29,14 @@ public class NewGuildJoined implements NewGuildJoinedHandler {
     @Override
     public void onGuildJoin(@Nonnull GuildJoinEvent event) {
         log.info("event: newguildjoined");
+        // Gets all the current channels in the Guild and checks if trading-channel already exists
         List<GuildChannel> guildChannels = event.getGuild().getChannels();
         for (GuildChannel guildChannel : guildChannels) {
             if ("trading-channel".equals(guildChannel.getName())) {
                 return;
             }
         }
+        // Created trading-channel if it does not yet exist
         event.getGuild().createTextChannel("trading-channel").complete();
     }
 }
