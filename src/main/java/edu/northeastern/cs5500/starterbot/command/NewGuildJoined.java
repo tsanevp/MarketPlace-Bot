e6e133fb027e8cd9a12f8c6a53ec3165b6f33746
Peter Tsanev <78775944+tsanevp@users.noreply.github.com>
2023-03-09 00:48:46 +0000

    Cleaned up NewGuildJoin class

--- src/main/java/edu/northeastern/cs5500/starterbot/command/NewGuildJoined.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/command/NewGuildJoined.java
@@ -1,7 +1,6 @@
 package edu.northeastern.cs5500.starterbot.command;
 
 import edu.northeastern.cs5500.starterbot.controller.UserController;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -19,7 +18,6 @@ import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.component.StringSelectInteractionEvent;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
-import net.dv8tion.jda.api.interactions.components.buttons.ButtonInteraction;
 import net.dv8tion.jda.api.interactions.components.selections.StringSelectMenu;
 import net.dv8tion.jda.api.interactions.components.selections.StringSelectMenu.Builder;
 import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
@@ -49,7 +47,7 @@ public class NewGuildJoined implements NewGuildJoinedHandler, ButtonHandler, Str
         EmbedBuilder embedBuilder =
                 new EmbedBuilder()
                         .setTitle(
-                                "Thank you for adding our MarketPlace Bot! For the bot to function as intended, a trading channel needs to be assigned. Would you like to create a new channel or use an existing channel in your server?")
+                                "Thank you for adding our MarketPlace Bot! To function as intended, a text channel to POST new listings needs to be assigned. Would you like to create a new channel or use an existing channel in your server?")
                         .setColor(0x00FFFF);
         MessageCreateBuilder messageCreateBuilder =
                 new MessageCreateBuilder()
@@ -70,60 +68,57 @@ public class NewGuildJoined implements NewGuildJoinedHandler, ButtonHandler, Str
         // Pulls the Guild ID from the user object NEED TO IMPLEMENT
         Guild guild = event.getJDA().getGuildById("1081855590650875925");
         List<TextChannel> guildChannels = guild.getTextChannels();
+        // Disable the buttons so that they can only be selected once
+        List<Button> buttons = new ArrayList<>();
+        for (Button button : event.getMessage().getButtons()) {
+            button = button.withDisabled(true);
+            buttons.add(button);
+        }
+        event.deferEdit().setActionRow(buttons).queue();
+        // Creates a new text channel named "trading-channel"
         if ("Create New Channel".equals(event.getButton().getLabel())) {
-            // Gets all the current channels in the Guild and checks if trading-channel already
-            // exists
+            // First checks if a channel named trading-channel already exists on the server
             for (GuildChannel guildChannel : guildChannels) {
                 if ("trading-channel".equals(guildChannel.getName())) {
-                    event.reply("\"trading-channel\" already exists on your server.").queue();;
+                    user.openPrivateChannel()
+                            .complete()
+                            .sendMessage("\"trading-channel\" already exists on your server.")
+                            .queue();
                     return;
                 }
             }
+            // Create new channel named "trading-channel" and added it to "Text Channels" grouping
             Category category = guild.getCategoriesByName("text channels", true).get(0);
             TextChannel textChannel = category.createTextChannel("trading-channel").complete();
             textChannel.getManager().setParent(category);
-            event.reply(
+            user.openPrivateChannel()
+                    .complete()
+                    .sendMessage(
                             String.format(
                                     "A new channel named \"trading-channel\" has been created in your server %s.",
                                     guild.getName()))
                     .queue();
         } else {
+            // Create a dropdown with all the existing channels a user can select as their trading
+            // channel
             Builder menu =
                     StringSelectMenu.create("newmember")
                             .setPlaceholder("Select Existing Channel To Assign For Sales");
             for (GuildChannel guildChannel : guildChannels) {
                 menu.addOption(guildChannel.getName(), guildChannel.getName());
             }
-
+            // Send an embed message with the channel dropdown
             EmbedBuilder embedBuilder =
                     new EmbedBuilder()
                             .setDescription(
                                     "Select which text channel you wish to assign as your trading channel.")
                             .setColor(0x00FFFF);
-            MessageCreateBuilder messageCreateBuilder = new MessageCreateBuilder();
-            messageCreateBuilder =
-                    messageCreateBuilder
+            MessageCreateBuilder messageCreateBuilder =
+                    new MessageCreateBuilder()
                             .mention(event.getUser())
                             .addActionRow(menu.build())
                             .addEmbeds(embedBuilder.build());
-            // event.deferReply().addActionRow(menu.build()).addEmbeds(embedBuilder.build()).queue();
-            // List<Button> buttons = new ArrayList<>();
-            
-            ButtonInteraction interaction = event.getInteraction();
-            // Button button = event.getButton();
-            // button = button.withDisabled(true);
-            // interaction.editButton(button).queue();
-            List<Button> buttons = new ArrayList<>();
-
-            for (Button button : event.getMessage().getButtons()) {
-                System.out.println(button.getId());
-                button = button.withDisabled(true);
-                buttons.add(button);
-            }
-            event.deferEdit().setActionRow(buttons).queue();
             user.openPrivateChannel().complete().sendMessage(messageCreateBuilder.build()).queue();
-
-
         }
     }
 
@@ -131,6 +126,7 @@ public class NewGuildJoined implements NewGuildJoinedHandler, ButtonHandler, Str
     public void onStringSelectInteraction(@Nonnull StringSelectInteractionEvent event) {
         // Here, need to instead save the drop down selection in the DB collection maintaining the
         // guild information
+        // The selection will be saved as the channel to POST new listing in
         final String response = event.getInteraction().getValues().get(0);
         Objects.requireNonNull(response);
         event.reply(response).queue();
