e30be20c5dd916fbf2ce1b0cf883c36414a54e46
Peter Tsanev <78775944+tsanevp@users.noreply.github.com>
2023-03-15 22:42:45 +0000

    Refactored UserController to user DBObjects

--- src/main/java/edu/northeastern/cs5500/starterbot/controller/UserController.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/controller/UserController.java
@@ -1,18 +1,15 @@
 package edu.northeastern.cs5500.starterbot.controller;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
 import com.mongodb.lang.Nullable;
 import edu.northeastern.cs5500.starterbot.model.User;
 import edu.northeastern.cs5500.starterbot.repository.GenericRepository;
-import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
-import javax.json.Json;
-import javax.json.JsonObject;
-import javax.json.JsonObjectBuilder;
-import javax.json.JsonReader;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.MessageEmbed.Field;
@@ -23,22 +20,13 @@ public class UserController {
     private static final String DESCRIPTION_FIELD_NAME = "Description:";
     private static final String IMAGE_FIELD_NAME = "image";
     private static final String COLOR_FIELD_NAME = "color";
+    private static final String FIELDS_NAME = "fields";
 
     GenericRepository<User> userRepository;
 
     @Inject
     UserController(GenericRepository<User> userRepository) {
         this.userRepository = userRepository;
-
-        if (userRepository.count() == 0) {
-            User user = new User();
-            user.setDiscordUserId("1234");
-            user.setGuildId("2349024");
-            user.setLocationOfResidence(null);
-            user.setCurrentListingAsString(null);
-            user.setCurrentListing(null);
-            userRepository.add(user);
-        }
     }
 
     public void setGuildIdForUser(String discordMemberId, String guildId) {
@@ -82,62 +70,61 @@ public class UserController {
         user.setCurrentListing(null);
         if (currentListings != null) {
             MessageEmbed currentListingAsBuilder = currentListings.get(0);
-            JsonObjectBuilder images = Json.createObjectBuilder();
             int i = 0;
+            DBObject imagesDbObject = new BasicDBObject();
             for (MessageEmbed messageEmbed : currentListings) {
-                images.add(IMAGE_FIELD_NAME + i, messageEmbed.getImage().getUrl());
+                imagesDbObject.put(IMAGE_FIELD_NAME + i, messageEmbed.getImage().getUrl());
                 i++;
             }
-            String imagesSaved = images.build().toString();
-            JsonObjectBuilder builder = Json.createObjectBuilder();
-            builder.add(TITLE_FIELD_NAME, currentListingAsBuilder.getTitle());
-            builder.add(TITLE_URL_FIELD_NAME, currentListingAsBuilder.getUrl());
-
+            DBObject embedDbObject =
+                    new BasicDBObject()
+                            .append(TITLE_FIELD_NAME, currentListingAsBuilder.getTitle())
+                            .append(TITLE_URL_FIELD_NAME, currentListingAsBuilder.getUrl())
+                            .append(IMAGE_FIELD_NAME, imagesDbObject)
+                            .append(COLOR_FIELD_NAME, currentListingAsBuilder.getColorRaw());
             List<Field> listingFields = currentListingAsBuilder.getFields();
+            DBObject fieldsDbObject = new BasicDBObject();
             for (Field field : listingFields) {
-                builder.add(field.getName(), field.getValue());
+                fieldsDbObject.put(field.getName(), field.getValue());
             }
-            builder.add(IMAGE_FIELD_NAME, imagesSaved);
-            builder.add(COLOR_FIELD_NAME, currentListingAsBuilder.getColorRaw());
-            JsonObject object = builder.build();
-            user.setCurrentListing(object.toString());
+            embedDbObject.put(FIELDS_NAME, fieldsDbObject);
+            user.setCurrentListing(embedDbObject);
         }
         userRepository.update(user);
     }
 
     @Nullable
     public List<MessageEmbed> getCurrentListing(String discordMemberId) {
-        JsonReader reader =
-                Json.createReader(
-                        new StringReader(getUserForMemberId(discordMemberId).getCurrentListing()));
-        JsonObject jsonObject = reader.readObject();
-        reader.close();
-
-        JsonReader reader2 =
-                Json.createReader(new StringReader(jsonObject.getString(IMAGE_FIELD_NAME)));
-        JsonObject jsonObject2 = reader2.readObject();
-        reader2.close();
-
+        DBObject currentObject = getUserForMemberId(discordMemberId).getCurrentListing();
+        DBObject imagesDbObject = (DBObject) currentObject.get(IMAGE_FIELD_NAME);
+        DBObject fieldsDbObject = (DBObject) currentObject.get(FIELDS_NAME);
         List<MessageEmbed> currentListings = new ArrayList<>();
-        for (int i = 0; i < jsonObject2.keySet().size(); i++) {
-            EmbedBuilder embedBuilder =
-                    new EmbedBuilder()
-                            .setColor(jsonObject.getInt(COLOR_FIELD_NAME))
-                            .setTitle(
-                                    jsonObject.getString(TITLE_FIELD_NAME),
-                                    jsonObject.getString(TITLE_URL_FIELD_NAME))
-                            .setImage(jsonObject2.getString(IMAGE_FIELD_NAME + i));
-            for (String key : jsonObject.keySet()) {
-                if (DESCRIPTION_FIELD_NAME.equals(key)) {
-                    embedBuilder.addField(key, jsonObject.getString(key), false);
-                } else if (!TITLE_FIELD_NAME.equals(key)
-                        && !TITLE_URL_FIELD_NAME.equals(key)
-                        && !IMAGE_FIELD_NAME.equals(key)
-                        && !COLOR_FIELD_NAME.equals(key)) {
-                    embedBuilder.addField(key, jsonObject.getString(key), true);
-                }
+        EmbedBuilder embedBuilder =
+                new EmbedBuilder()
+                        .setColor((int) currentObject.get(COLOR_FIELD_NAME))
+                        .setTitle(
+                                (String) currentObject.get(TITLE_FIELD_NAME),
+                                (String) currentObject.get(TITLE_URL_FIELD_NAME))
+                        .setImage((String) imagesDbObject.get(IMAGE_FIELD_NAME + 0));
+        for (String key : fieldsDbObject.keySet()) {
+            if (DESCRIPTION_FIELD_NAME.equals(key)) {
+                embedBuilder.addField(key, (String) fieldsDbObject.get(key), false);
+                continue;
             }
-            currentListings.add(embedBuilder.build());
+            embedBuilder.addField(key, (String) fieldsDbObject.get(key), true);
+        }
+
+        MessageEmbed messageEmbed = embedBuilder.build();
+        currentListings.add(messageEmbed);
+        if (imagesDbObject.keySet().size() > 1) {
+            for (int i = 1; i < imagesDbObject.keySet().size(); i++) {
+                EmbedBuilder embedBuilders =
+                        new EmbedBuilder()
+                                .setColor(messageEmbed.getColorRaw())
+                                .setTitle(messageEmbed.getTitle(), messageEmbed.getUrl())
+                                .setImage((String) imagesDbObject.get(IMAGE_FIELD_NAME + i));
+                currentListings.add(embedBuilders.build());
+            }
         }
         return currentListings;
     }
