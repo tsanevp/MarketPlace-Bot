a58334ed6e433d57895b0ca05713c5da2e07b387
lenad90 <lenatduong@gmail.com>
2023-03-20 19:37:54 +0000

    created collection format for listing

--- src/main/java/edu/northeastern/cs5500/starterbot/model/ListingCollection.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/model/ListingCollection.java
@@ -1,8 +1,27 @@
 package edu.northeastern.cs5500.starterbot.model;
 import lombok.Data;
+
+import java.util.List;
+
 import org.bson.types.ObjectId;
+
+import com.mongodb.DBObject;
 
 @Data
 public class ListingCollection implements Model{
     ObjectId id;
+    
+    String discordUserId;
+
+    String title;
+
+    String url;
+
+    String description;
+
+    List<String> images;
+
+    String color;
+
+    String fields;
 }
