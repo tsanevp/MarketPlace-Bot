0d43470027af875787cb07485a9f027072005a33
lenad90 <lenatduong@gmail.com>
2023-03-30 06:10:05 +0000

    refractor delete button and deletes listing from trading-channel

--- src/main/java/edu/northeastern/cs5500/starterbot/command/ViewMyListingCommand.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/command/ViewMyListingCommand.java
@@ -4,16 +4,21 @@ import com.mongodb.client.FindIterable;
 import edu.northeastern.cs5500.starterbot.controller.ListingController;
 import edu.northeastern.cs5500.starterbot.controller.UserController;
 import edu.northeastern.cs5500.starterbot.model.Listing;
+import java.util.Objects;
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import lombok.extern.slf4j.Slf4j;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import net.dv8tion.jda.api.requests.restaction.interactions.MessageEditCallbackAction;
 import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
 import org.bson.types.ObjectId;
 
@@ -65,12 +70,12 @@ public class ViewMyListingCommand implements SlashCommandHandler, ButtonHandler 
                                                         + ":delete",
                                                 "Delete"))
                                 .setEmbeds(this.listingController.toMessageEmbed(list));
-                log.info("in for loop");
                 event.getUser()
                         .openPrivateChannel()
                         .complete()
                         .sendMessage(messageCreateBuilder.build())
                         .queue();
+                // event.reply(messageCreateBuilder.build()).setEphemeral(true).queue();
             }
             event.reply("Your postings has been sent to your DM").setEphemeral(true).complete();
         } else {
@@ -81,8 +86,23 @@ public class ViewMyListingCommand implements SlashCommandHandler, ButtonHandler 
     @Override
     public void onButtonInteraction(@Nonnull ButtonInteractionEvent event) {
         User user = event.getUser();
+        Guild guild =
+                Objects.requireNonNull(
+                        event.getJDA()
+                                .getGuildById((userController.getGuildIdForUser(user.getId()))));
+        MessageChannel channel =
+                guild.getTextChannelsByName(userController.getTradingChannel(user.getId()), true)
+                        .get(0);
+        MessageEditCallbackAction buttonEvent = event.deferEdit().setComponents();
         String[] buttonIds = event.getButton().getId().split(":");
         this.listingController.deleteListingById(new ObjectId(buttonIds[2]));
-        event.reply("Your post has been successfully deleted").complete();
+        channel.deleteMessageById(buttonIds[1]).queue();
+        buttonEvent
+                .setEmbeds(
+                        new EmbedBuilder()
+                                .setDescription("Your post has been successfully deleted")
+                                .setColor(EMBED_COLOR)
+                                .build())
+                .queue();
     }
 }
