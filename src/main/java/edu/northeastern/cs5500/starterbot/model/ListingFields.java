f59865ed3db02a8580e9d5384056093a2b707a2c
lenad90 <lenatduong@gmail.com>
2023-04-13 23:54:54 +0000

    capitalized comments

--- src/main/java/edu/northeastern/cs5500/starterbot/model/ListingFields.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/model/ListingFields.java
@@ -13,18 +13,18 @@ import lombok.Data;
 @Builder
 public class ListingFields {
 
-    // cost of item
+    // Cost of item
     @Nonnegative int cost;
 
-    // whether shipping is included in the cost of item
+    // Whether shipping is included in the cost of item
     boolean shippingIncluded;
 
-    // condition of the item
+    // Condition of the item
     @Nonnull String condition;
 
-    // description of item listed
+    // Description of item listed
     @Nonnull String description;
 
-    // original date posted of the listing
+    // Original date posted of the listing
     @Nonnull String datePosted;
 }
